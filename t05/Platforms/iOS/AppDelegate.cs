a6c3e5b1617d6941a04441cda94ca26494c93d02
Jussi17 <jus.jj@hotmail.com>
2025-03-06 12:14:06 +0200

    7+8

--- t05/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace t05;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
---
+++ t07/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace t07
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
