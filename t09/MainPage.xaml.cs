e8c98d145ea4b64d74ff33c304400c34d64ae4f4
Jussi17 <jus.jj@hotmail.com>
2025-03-07 11:43:42 +0200

    9+10

---
+++ t09/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
+
+namespace t09
+{
+    public partial class MainPage : ContentPage
+    {
+        private Random _random = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPageClicked(object sender, EventArgs e)
+        {
+            if(BackgroundColor != Colors.Red)
+            {
+                BackgroundColor = Colors.Red;
+            }
+            else
+            {
+                BackgroundColor = RandomColor();
+            }
+        }
+
+        private Color RandomColor()
+        {
+            return Color.FromRgb(_random.Next(256), _random.Next(256), _random.Next(256));
+        }
+    }
+}
---
+++ t10/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Storage;
+
+namespace t08
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnOpenClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await FilePicker.PickAsync(new PickOptions
+                {
+                    PickerTitle = "Valitse kuvatiedosto",
+                    FileTypes = FilePickerFileType.Images
+                });
+
+                if (result != null)
+                {
+                    using var stream = await result.OpenReadAsync();
+                    var memoryStream = new MemoryStream();
+                    await stream.CopyToAsync(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    SelectedImage.Source = ImageSource.FromStream(() => memoryStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Virhe", $"Kuvan valinta epäonnistui: {ex.Message}", "OK");
+            }
+        }
+    }
+}
